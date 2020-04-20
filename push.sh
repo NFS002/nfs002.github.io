a8d9c02934b1ab8c93d978dd2e8efc1276a073f0
Noah  Florin-Sefton <noah@otis-app.com>
2020-04-21 00:49:29 +0100

    updated theme

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git commit -m "updated theme"
 git push origin sources
 bundle exec jekyll build
 echo "skydesign.blue" > _site/CNAME
-push-dir --dir=_site --branch=maste
+push-dir --dir=_site --branch=master
